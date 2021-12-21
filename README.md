d6f139020098d78aa0230fa8d973c363a7e195c5
eyalz800 <eyalz800@gmail.com>
2021-12-21 04:32:43 +0200

    Added a way to do explicit versioning via variant, and additional constexpr convenient facilities for compile time.

--- README.md
+++ README.md
@@ -405,6 +405,23 @@ The example above works with or without `ZPP_BITS_AUTODETECT_MEMBERS_MODE=1`, de
 on the `#if`. As noted above, we must rely on specific compiler feature to detect the
 number of members which may not be portable.
 
+* For convenience, the library also provides some simplified serialization functions for
+compile time:
+```cpp
+using namespace zpp::bits::literals;
+
+// Returns an array
+// where the first bytes are those of the hello world string and then
+// the 1337 as 4 byte integer.
+constexpr std::array data =
+    zpp::bits::to_bytes<"Hello World!"_s, 1337>();
+
+static_assert(
+    zpp::bits::from_bytes<data,
+                          zpp::bits::string_literal<char, 12>,
+                          int>() == std::tuple{"Hello World!"_s, 1337});
+```
+
 * This should cover most of the basic stuff, more documentation may come in the future.
 
 Limitations
--- test/src/test_variant.cpp
+++ test/src/test_variant.cpp
@@ -1,5 +1,10 @@
 #include "test.h"
 
+namespace test_variant
+{
+using namespace std::literals;
+using namespace zpp::bits::literals;
+
 TEST(variant, int)
 {
     auto [data, in, out] = zpp::bits::data_in_out();
@@ -32,3 +37,349 @@ TEST(variant, string)
     EXPECT_TRUE(std::holds_alternative<std::string>(v));
     EXPECT_EQ(std::get<std::string>(v), "1234");
 }
+
+namespace string_versioning::v1
+{
+struct person
+{
+    using serialize = zpp::bits::members<2>;
+    using serialize_id = zpp::bits::id<"v1::person"_s>;
+
+    auto get_hobby() const
+    {
+        return "<none>"sv;
+    }
+
+    std::string name;
+    int age;
+};
+} // namespace v1
+
+namespace string_versioning::v2
+{
+struct person
+{
+    using serialize = zpp::bits::members<3>;
+    using serialize_id = zpp::bits::id<"v2::person"_s>;
+
+    auto get_hobby() const
+    {
+        return std::string_view(hobby);
+    }
+
+    std::string name;
+    int age;
+    std::string hobby;
+};
+} // namespace v2
+
+TEST(variant, string_versioning_v1)
+{
+    using namespace string_versioning;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(v1::person{"Person1", 25}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<"v1::person"_s,
+                                    "Person1"_s.size(),
+                                    "Person1"_s,
+                                    25>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person1");
+        EXPECT_EQ(person.age, 25);
+        EXPECT_EQ(person.get_hobby(), "<none>"sv);
+    }, v);
+}
+
+TEST(variant, string_versioning_v2)
+{
+    using namespace string_versioning;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(
+            v2::person{"Person2", 35, "Basketball"}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<"v2::person"_s,
+                                    "Person2"_s.size(),
+                                    "Person2"_s,
+                                    35,
+                                    "Basketball"_s.size(),
+                                    "Basketball"_s>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person2");
+        EXPECT_EQ(person.age, 35);
+        EXPECT_EQ(person.get_hobby(), "Basketball"sv);
+    }, v);
+}
+
+namespace string_versioning_outside::v1
+{
+struct person
+{
+    auto get_hobby() const
+    {
+        return "<none>"sv;
+    }
+
+    std::string name;
+    int age;
+};
+
+auto serialize(const person &) -> zpp::bits::members<2>;
+auto serialize_id(const person &) -> zpp::bits::id<"v1::person"_s>;
+} // namespace v1
+
+namespace string_versioning_outside::v2
+{
+struct person
+{
+    auto get_hobby() const
+    {
+        return std::string_view(hobby);
+    }
+
+    std::string name;
+    int age;
+    std::string hobby;
+};
+
+auto serialize(const person &) -> zpp::bits::members<3>;
+auto serialize_id(const person &) -> zpp::bits::id<"v2::person"_s>;
+} // namespace v2
+
+TEST(variant, string_versioning_outside_v1)
+{
+    using namespace string_versioning_outside;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(v1::person{"Person1", 25}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<"v1::person"_s,
+                                    "Person1"_s.size(),
+                                    "Person1"_s,
+                                    25>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person1");
+        EXPECT_EQ(person.age, 25);
+        EXPECT_EQ(person.get_hobby(), "<none>"sv);
+    }, v);
+}
+
+TEST(variant, string_versioning_outside_v2)
+{
+    using namespace string_versioning_outside;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(
+            v2::person{"Person2", 35, "Basketball"}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<"v2::person"_s,
+                                    "Person2"_s.size(),
+                                    "Person2"_s,
+                                    35,
+                                    "Basketball"_s.size(),
+                                    "Basketball"_s>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person2");
+        EXPECT_EQ(person.age, 35);
+        EXPECT_EQ(person.get_hobby(), "Basketball"sv);
+    }, v);
+}
+
+namespace int_versioning::v1
+{
+struct person
+{
+    using serialize = zpp::bits::members<2>;
+    using serialize_id = zpp::bits::id<1337>;
+
+    auto get_hobby() const
+    {
+        return "<none>"sv;
+    }
+
+    std::string name;
+    int age;
+};
+} // namespace v1
+
+namespace int_versioning::v2
+{
+struct person
+{
+    using serialize = zpp::bits::members<3>;
+    using serialize_id = zpp::bits::id<1338>;
+
+    auto get_hobby() const
+    {
+        return std::string_view(hobby);
+    }
+
+    std::string name;
+    int age;
+    std::string hobby;
+};
+} // namespace v2
+
+TEST(variant, int_versioning_v1)
+{
+    using namespace int_versioning;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(v1::person{"Person1", 25}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<1337,
+                                    "Person1"_s.size(),
+                                    "Person1"_s,
+                                    25>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person1");
+        EXPECT_EQ(person.age, 25);
+        EXPECT_EQ(person.get_hobby(), "<none>"sv);
+    }, v);
+}
+
+TEST(variant, int_versioning_v2)
+{
+    using namespace int_versioning;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(
+            v2::person{"Person2", 35, "Basketball"}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<1338,
+                                    "Person2"_s.size(),
+                                    "Person2"_s,
+                                    35,
+                                    "Basketball"_s.size(),
+                                    "Basketball"_s>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person2");
+        EXPECT_EQ(person.age, 35);
+        EXPECT_EQ(person.get_hobby(), "Basketball"sv);
+    }, v);
+}
+
+namespace int_versioning_outside::v1
+{
+struct person
+{
+    auto get_hobby() const
+    {
+        return "<none>"sv;
+    }
+
+    std::string name;
+    int age;
+};
+
+auto serialize(const person &) -> zpp::bits::members<2>;
+auto serialize_id(const person &) -> zpp::bits::id<1337>;
+} // namespace v1
+
+namespace int_versioning_outside::v2
+{
+struct person
+{
+    auto get_hobby() const
+    {
+        return std::string_view(hobby);
+    }
+
+    std::string name;
+    int age;
+    std::string hobby;
+};
+
+auto serialize(const person &) -> zpp::bits::members<3>;
+auto serialize_id(const person &) -> zpp::bits::id<1338>;
+} // namespace v2
+
+TEST(variant, int_versioning_outside_v1)
+{
+    using namespace int_versioning_outside;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(v1::person{"Person1", 25}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<1337,
+                                    "Person1"_s.size(),
+                                    "Person1"_s,
+                                    25>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person1");
+        EXPECT_EQ(person.age, 25);
+        EXPECT_EQ(person.get_hobby(), "<none>"sv);
+    }, v);
+}
+
+TEST(variant, int_versioning_outside_v2)
+{
+    using namespace int_versioning_outside;
+    auto [data, in, out] = zpp::bits::data_in_out();
+    out(std::variant<v1::person, v2::person>(
+            v2::person{"Person2", 35, "Basketball"}))
+        .or_throw();
+
+    EXPECT_EQ(
+        hexlify(data),
+        hexlify(zpp::bits::to_bytes<1338,
+                                    "Person2"_s.size(),
+                                    "Person2"_s,
+                                    35,
+                                    "Basketball"_s.size(),
+                                    "Basketball"_s>()));
+
+    std::variant<v1::person, v2::person> v;
+    in(v).or_throw();
+
+    std::visit([](auto && person) {
+        EXPECT_EQ(person.name, "Person2");
+        EXPECT_EQ(person.age, 35);
+        EXPECT_EQ(person.get_hobby(), "Basketball"sv);
+    }, v);
+}
+} // namespace test_variant
+
