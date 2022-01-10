7b9ee4500237b817588a960ad4009ffbf41de504
eyalz800 <eyalz800@gmail.com>
2022-01-10 02:35:39 +0200

    Bitset support.

---
+++ test/src/test_bitset.cpp
@@ -0,0 +1,21 @@
+#include "test.h"
+#include <bitset>
+
+namespace test_bitset
+{
+
+TEST(test_bitset, sanity)
+{
+    auto [data, in, out] = zpp::bits::data_in_out();
+    constexpr std::bitset<8> o(0x7f);
+    out(o).or_throw();
+    EXPECT_EQ(out.position(), 1u);
+
+    std::bitset<8> i;
+    in(i).or_throw();
+
+    EXPECT_EQ(in.position(), 1u);
+    EXPECT_EQ(i, i);
+}
+
+} // namespace test_bitset
