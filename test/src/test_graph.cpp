c89eb94d0199fc7712fa8826eb968490e080c07d
eyalz800 <eyalz800@gmail.com>
2022-01-19 12:45:38 +0200

    Added test for tree graph.

---
+++ test/src/test_graph.cpp
@@ -0,0 +1,52 @@
+#include "test.h"
+
+namespace test_graph
+{
+
+struct node
+{
+    int value;
+    std::vector<node> nodes;
+};
+
+TEST(test_graph, tree)
+{
+    auto [data, in, out] = zpp::bits::data_in_out();
+    node o =
+        {1,
+            std::vector {
+                node {
+                    2,
+                    std::vector {
+                        node {
+                            3,
+                            std::vector {
+                                node{4, {}}
+                            }
+                        }
+                    }
+                },
+                node {5, {}}
+            }
+        };
+    out(o).or_throw();
+
+    node i;
+    in(i).or_throw();
+
+    EXPECT_EQ(o.value, i.value);
+    EXPECT_EQ(o.nodes.size(), i.nodes.size());
+
+    EXPECT_EQ(i.value, 1);
+    ASSERT_EQ(i.nodes.size(), 2u);
+    EXPECT_EQ(i.nodes[0].value, 2);
+    ASSERT_EQ(i.nodes[0].nodes.size(), 1u);
+    EXPECT_EQ(i.nodes[0].nodes[0].value, 3);
+    ASSERT_EQ(i.nodes[0].nodes[0].nodes.size(), 1u);
+    EXPECT_EQ(i.nodes[0].nodes[0].nodes[0].value, 4);
+    EXPECT_EQ(i.nodes[0].nodes[0].nodes[0].nodes.size(), 0u);
+    EXPECT_EQ(i.nodes[1].value, 5);
+    ASSERT_EQ(i.nodes[1].nodes.size(), 0u);
+}
+
+} // namespace test_graph
